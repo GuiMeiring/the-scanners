85e5525d4923e1ed9c2cfe22abbede8b53ba32a8
GuiMeiring <guilhermemeiring08@gmail.com>
2023-02-20 15:44:30 -0300

    .

--- server1/config/env-development.js
+++ server1/config/env-development.js
@@ -1,4 +1,4 @@
 process.env.DB_SERVER   = 'localhost'; 
 process.env.DB_USER     = 'root'     ;
-process.env.DB_PASS     = 'root' ;
+process.env.DB_PASS     = 'gui082005' ;
 process.env.DB_DATABASE = 'guilhermemeiring';
