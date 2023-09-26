d1784b1df94017bebef57cece1716e5100fa4c85
Heloisa <102254183+heloisacamili@users.noreply.github.com>
2023-09-26 00:15:57 -0300

    add: pages with ESlint (google)

--- server1/index.js
+++ server1/index.js
@@ -1,5 +1,3 @@
-const { required } = require('joi');
-
 require('./src/config/env-config');
 require('./src/starters/start-async-storange');
 require('./src/starters/start-express');
@@ -15,13 +13,20 @@ const swaggerDocument = require('./src/swagger.json');
 
 let port = 3007;
 
-if (process.env.PORT){
-    port = process.env.PORT;
+if (process.env.PORT) {
+  port = process.env.PORT;
 }
 
-global.app.express.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+global.app.express.use(
+    '/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument),
+);
 
 global.app.express_listener_instance = global.app.express.listen(port, () => {
-    console.log('Express is listen on ' + global.app.express_listener_instance.address().port)
-    console.log("Documentation: http://localhost:"+global.app.express_listener_instance.address().port+"/api-docs/")
+  console.log(
+      'Express is listen on'+global.app.express_listener_instance.address().
+          port,
+  );
+  console.log(
+      'Documentation: http://localhost:'+global.app.express_listener_instance.address().port+'/api-docs/',
+  );
 });
--- server1/src/routes/group.js
+++ server1/src/routes/group.js
@@ -1,73 +1,69 @@
 const Joi=require('joi');
 const knl =require('../knl');
-const securityConsts = require('../consts/security-consts');
 
-knl.post('group', async(req, resp) =>{
-    const schema =Joi.object({
-        description:
-        Joi.string().min(1).max(100).required(),
-
-    })
-    knl.validate(req.body, schema);
-
-    const result = await knl.sequelize().models.Group.findAll({
-        where:{
-            description: req.body.description
-        }
-    });
-
-    knl.createException('0006', '', !knl.objects.isEmptyArray(result));
-
-    const group =knl.sequelize().models.Group.build({
-        description: req.body.description,
-        status:1
-    });
-   
-    await group.save();
-    await resp.json({ "status": "OK" });
-    resp.end()
-
-})
-knl.get('group', async (req,resp)=>{
-    const result =await knl.sequelize().models.Group.findAll({
-        where: {
-            status:1
-        }
-    });
-    
-    resp.json(result);
-})
-knl.get('group/:id', async (req,resp)=>{
-    const result =await knl.sequelize().models.Group.findAll({
-    where:{
-        id:req.params.id
-        }
-    });
-    resp.json(result);
-})
-knl.patch('group/:id', async(req,resp)=>{
-    console.log(req.params.id);
-    const result = await knl.sequelize().models.Group.update({
-        status:0
+knl.post('group', async (req, resp) => {
+  const schema = Joi.object({
+    description: Joi.string().min(1).max(100).required(),
+  });
+  knl.validate(req.body, schema);
+  const result = await knl.sequelize().models.Group.findAll({
+    where: {
+      description: req.body.description,
     },
-    {
-    where:{
-        id:req.params.id
-        }
-    });
-    resp.json({"status":"OK"});
-    resp.end();
-})
-knl.put('group', async(req, resp) => {
-    const result = await knl.sequelize().models.Group.update({
-        description:req.body.description,
-    }, {
-        where : {
-        id : req.body.id
-    }});
-    
-    
-    resp.json({"status":"OK"});
-    resp.end();
-    
+  });
+  knl.createException('0006', '', !knl.objects.isEmptyArray(result));
+  const group = knl.sequelize().models.Group.build({
+    description: req.body.description,
+    status: 1,
+  }); 
+  await group.save();
+  await resp.json({'status': 'OK'});
+  resp.end();
+});
+
+knl.get('group', async (resp) => {
+  const result =await knl.sequelize().models.Group.findAll({
+    where: {
+      status: 1,
+    },
+  });  
+  resp.json(result);
+});
+
+knl.get('group/:id', async (req, resp) => {
+  const result =await knl.sequelize().models.Group.findAll({
+    where: {
+      id: req.params.id,
+    },
+  });
+  resp.json(result);
+});
+
+knl.patch('group/:id', async (req, resp) => {
+  console.log(req.params.id);
+  const result = await knl.sequelize().models.Group.update({
+    status: 0,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  });
+  resp.json({'status': 'OK'});
+  resp.save(result);
+  resp.end();
+});
+
+knl.put('group', async (req, resp) => {
+  const result = await knl.sequelize().models.Group.update({
+    description: req.body.description,
+  }, 
+  {
+    where: {
+      id: req.body.id,
+    },
+  });  
+  resp.json({'status': 'OK'});
+  resp.save(result);
+  resp.end();  
 });
